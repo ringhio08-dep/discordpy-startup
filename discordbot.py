9f4323e8179b48b86862289bfc9f9928228e6123
ringhio08-dep <62052604+ringhio08-dep@users.noreply.github.com>
2020-04-21 17:09:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,28 +1,10 @@
-import discord
-import configparser
+from discord.ext import commands
 import os
-import csv
-import pandas as pd
-import unicodedata
-import asyncio
-import re
 import traceback
 
-from discord.ext import commands
-from datetime import datetime 
-from discord.ext import tasks
-
-#設定ファイルから情報を格納
-INIFILE = configparser.ConfigParser()
-INIFILE.read('config.ini', 'UTF-8')
-CHANNEL_ID = int(INIFILE.get('Discord', 'CHANNEL'))
-COMAND_PREFIX = INIFILE.get('Discord', 'COMAND_PREFIX')
-DEL_TIME = int(INIFILE.get('Discord', 'DEL_TIME'))
-
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-bot = commands.Bot(command_prefix= COMAND_PREFIX)
-send_channel = ""
-notes = ""
+
 
 @bot.event
 async def on_command_error(ctx, error):
