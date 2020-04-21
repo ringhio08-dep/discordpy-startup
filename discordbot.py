240d4ab6226597dd0f8caac05ec01eb7e0edee57
ringhio08-dep <62052604+ringhio08-dep@users.noreply.github.com>
2020-04-21 17:12:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,14 @@
-from discord.ext import commands
+import discord
+import configparser
+import csv
+import pandas as pd
+import unicodedata
+import asyncio
+import re
 import os
 import traceback
+
+from discord.ext import commands
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
